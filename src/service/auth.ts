49828692835678d58d3f73dc01f544a999211f64
StarPandaBeg <kshmurakov@mail.ru>
2024-06-25 22:28:29 +0300

    feat: add auth service & storage

---
+++ src/service/auth.ts
@@ -0,0 +1,16 @@
+import type { AuthCredentials, AuthRefreshCredentials } from "@/api/auth";
+
+import * as authApi from "@/api/auth";
+import { authStorage } from "../lib/storage";
+
+export async function signIn(credentials: AuthCredentials) {
+  const result = await authApi.auth(credentials);
+  authStorage().set(result);
+  return result;
+}
+
+export async function refresh(credentials: AuthRefreshCredentials) {
+  const result = await authApi.refresh(credentials);
+  authStorage().set(result);
+  return result;
+}
