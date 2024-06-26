e967eb7e97fe9a5c1ea63e7f445e9640f3f12bff
StarPandaBeg <kshmurakov@mail.ru>
2024-06-26 16:43:03 +0300

    feat: add /sign-out route

---
+++ src/app/(auth)/sign-out/route.ts
@@ -0,0 +1,13 @@
+import { authStorage } from "@/lib/storage";
+import { logout } from "@/service/auth";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { NextRequest, NextResponse } from "next/server";
+
+const handler = async (request: NextRequest, response: NextResponse) => {
+  await logout();
+  authStorage(cookies()).remove();
+  return redirect("/sign-in");
+};
+
+export { handler as GET };
