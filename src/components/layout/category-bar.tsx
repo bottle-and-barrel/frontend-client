c9da471d878ed8bf940276d96d82d457328cc9fd
StarPandaBeg <kshmurakov@mail.ru>
2024-06-28 00:44:39 +0300

    feat: add category bar
    - show bar only on desktop devices
    - fetch categories from api
    - show skeleton while loading

--- src/components/layout/category-bar.tsx
+++ src/components/layout/category-bar.tsx
@@ -1,3 +1,38 @@
-export default function CategoryBar() {
-  return <nav></nav>;
+import { Category } from "@/service/category";
+import { NavigationMenuProps } from "@radix-ui/react-navigation-menu";
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "../ui/navigation-menu";
+import { ScrollArea } from "../ui/scroll-area";
+
+export interface CategoryBarProps extends NavigationMenuProps {
+  categories: Category[];
+}
+
+export default function CategoryBar({
+  categories,
+  ...props
+}: CategoryBarProps) {
+  return (
+    <NavigationMenu {...props}>
+      <ScrollArea type="auto" useHorizontalScroll>
+        <NavigationMenuList className="pb-4">
+          {categories.map((c, i) => (
+            <NavigationMenuItem key={i}>
+              <Link href={c.link} legacyBehavior passHref>
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  {c.name}
+                </NavigationMenuLink>
+              </Link>
+            </NavigationMenuItem>
+          ))}
+        </NavigationMenuList>
+      </ScrollArea>
+    </NavigationMenu>
+  );
 }
--- src/components/layout/page-header.tsx
+++ src/components/layout/page-header.tsx
@@ -1,18 +1,44 @@
 "use client";
 
+import { all } from "@/service/category";
+import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import { Skeleton } from "../ui/skeleton";
+import LoadingWrapper from "../util/loading-wrapper";
 import ActionBar from "./action-bar";
 import CategoryBar from "./category-bar";
 import PageBottomNavigation from "./page-bottom-navigation";
 import PageMenu from "./page-menu";
 
+function CategoryBarSkeleton() {
+  return (
+    <div className="px-2 py-4 flex gap-4">
+      <Skeleton className="h-4 w-[80px]" />
+      <Skeleton className="h-4 w-[120px]" />
+      <Skeleton className="h-4 w-[95px]" />
+      <Skeleton className="h-4 w-[65px]" />
+    </div>
+  );
+}
+
 export default function PageHeader() {
   const [menuOpened, setMenuOpened] = useState(false);
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["categories"],
+    queryFn: all,
+  });
 
   return (
     <header>
       <ActionBar setMenuOpened={setMenuOpened} />
-      <CategoryBar />
+      <LoadingWrapper
+        isLoading={isLoading}
+        isError={isError}
+        skeleton={<CategoryBarSkeleton />}
+      >
+        <CategoryBar className="mt-2 hidden sm:block" categories={data || []} />
+      </LoadingWrapper>
       <PageMenu open={menuOpened} onOpenChange={setMenuOpened}>
         This is a sample text
       </PageMenu>
