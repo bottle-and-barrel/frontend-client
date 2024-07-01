ee6951763c00e8dc827c1ae198b4a196539c2924
StarPandaBeg <kshmurakov@mail.ru>
2024-07-01 12:42:08 +0300

    feat: add react-query stale time

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -4,6 +4,7 @@ import { AxiosError } from "axios";
 export const clientConfig = {
   defaultOptions: {
     queries: {
+      staleTime: 60 * 1000,
       throwOnError(error, query) {
         if (error instanceof AxiosError) return false;
 
