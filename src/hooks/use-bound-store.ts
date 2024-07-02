5e0618c28db17caab2c6fdd8c6432c8ecf319b59
StarPandaBeg <kshmurakov@mail.ru>
2024-07-02 18:55:12 +0300

    fix: persist app state in localStorage

--- src/hooks/use-bound-store.ts
+++ src/hooks/use-bound-store.ts
@@ -1,17 +1,21 @@
 import { create } from "zustand";
 import { syncTabs } from "zustand-sync-tabs";
+import { persist } from "zustand/middleware";
 import createFavoriteSlice, { FavoriteSlice } from "../store/favorite";
 
 type StoreType = FavoriteSlice;
 
 const useBoundStore = create<StoreType>()(
-  syncTabs(
-    (...a) => ({
-      ...createFavoriteSlice(...a),
-    }),
-    {
-      name: "app-storage",
-    }
+  persist(
+    syncTabs(
+      (...a) => ({
+        ...createFavoriteSlice(...a),
+      }),
+      {
+        name: "app-storage",
+      }
+    ),
+    { name: "app-storage" }
   )
 );
 
