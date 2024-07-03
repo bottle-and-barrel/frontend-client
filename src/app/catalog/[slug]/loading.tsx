80e1c501680198341e7749636810601b94a7af4d
StarPandaBeg <kshmurakov@mail.ru>
2024-07-03 22:31:47 +0300

    refactor: remove product-list-skeleton

--- src/app/catalog/[slug]/loading.tsx
+++ src/app/catalog/[slug]/loading.tsx
@@ -1,7 +1,10 @@
-import ProductListSkeleton from "@/components/products/product-list-skeleton";
+import ProductCardSkeleton from "@/components/products/product-card-skeleton";
+import CardList from "@/components/ui/card-list";
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function CatalogCategoryViewLoading() {
+  const cards = [...Array(8)];
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-[300px_1fr]">
       <section></section>
@@ -12,7 +15,7 @@ export default function CatalogCategoryViewLoading() {
             Мы нашли для Вас 0 напитков
           </p>
         </div>
-        <ProductListSkeleton />
+        <CardList items={cards} itemBase={ProductCardSkeleton} />
       </section>
     </div>
   );
