6e65f69b8b9b1bbc309c3e2f7378c5549abe32d2
StarPandaBeg <kshmurakov@mail.ru>
2024-07-03 23:03:19 +0300

    feat: add products service
    temporary mock all data

--- src/api/category.ts
+++ src/api/category.ts
@@ -1,4 +1,4 @@
-import { Product } from "./product";
+import { Product, products } from "./product";
 
 export interface Category {
   name: string;
@@ -10,18 +10,7 @@ const categories = [
   {
     name: "Вино",
     slug: "wine",
-    products: [
-      {
-        id: 1,
-        name: "Вино Shiraz Mr Borio's, Simonsig, 2021",
-        description:
-          "Светло-соломенный цвет с зеленоватыми оттенками. Аромат с тонами цветов, миндаля и экзотических фруктов. Сбалансированное, освежающее вино с гармоничной кислотностью, гармоничным сочетанием фруктового вкуса и минеральной солёности в послевкусии.",
-        price: 2490,
-        sku: "147564",
-        slug: "shiraz_mr_borio_s_2021",
-        status: "visible",
-      },
-    ],
+    products,
   },
   { name: "Шампанское и игристое", slug: "champagne", products: [] },
   { name: "Виски", slug: "whiskey", products: [] },
---
+++ src/service/product.ts
@@ -0,0 +1,22 @@
+import * as productApi from "@/api/product";
+
+export const KEY = "product";
+
+export interface Product extends productApi.Product {
+  link: string;
+}
+
+function mapProduct(product: productApi.Product) {
+  return { ...product, link: `/products/${product.slug}` } as Product;
+}
+
+export async function all() {
+  const products = await productApi.all();
+  return products.map(mapProduct);
+}
+
+export async function getById(id: number) {
+  const product = await productApi.getById(id);
+  if (!product) return null;
+  return mapProduct(product);
+}
