24d042b9f78f06085d22dbaee88fcd82b1c14207
StarPandaBeg <kshmurakov@mail.ru>
2024-07-03 22:46:25 +0300

    refactor: split favorite state in the separate component

--- src/components/products/product-card.tsx
+++ src/components/products/product-card.tsx
@@ -4,7 +4,7 @@ import { Product } from "@/api/product";
 import useFavorite from "@/hooks/use-favorite";
 import { cn } from "@/lib/util";
 import { HeartIcon } from "lucide-react";
-import { HTMLAttributes, useState } from "react";
+import { HTMLAttributes, MouseEvent, useState } from "react";
 import { Badge } from "../ui/badge";
 import { Button } from "../ui/button";
 import ImageWithFallback from "../util/image-with-fallback";
@@ -13,15 +13,43 @@ export interface ProductCardProps extends HTMLAttributes<HTMLDivElement> {
   item: Product;
 }
 
+function ProductCardFavorite({ item }: { item: Product }) {
+  const [isFavorite, setFavorite] = useFavorite(item.id);
+  const [heartBeat, setHeartbeat] = useState(false);
+
+  const elementClass =
+    "text-primary/30 transition hover:text-primary/50 hover:scale-110";
+
+  const clickHandler = (e: MouseEvent) => {
+    setHeartbeat(true);
+    setFavorite(!isFavorite);
+  };
+
+  return (
+    <div className="absolute right-4 top-4 z-10">
+      <Button
+        variant="text"
+        size="icon"
+        title="Добавить в избранное"
+        className={cn(
+          elementClass,
+          heartBeat && "animate-[heart-pulse_ease-in-out_0.5s]"
+        )}
+        onClick={clickHandler}
+        onAnimationEnd={(e) => setHeartbeat(false)}
+      >
+        <HeartIcon fill={isFavorite ? "hsl(var(--accent))" : "transparent"} />
+      </Button>
+    </div>
+  );
+}
+
 export default function ProductCard({
   item,
   className,
   ...props
 }: ProductCardProps) {
   const priceWithoutDiscount = item.price * 1.4;
-  const [isFavorite, setFavorite] = useFavorite(item.id);
-
-  const [heartBeat, setHeartbeat] = useState(false);
 
   return (
     <div
@@ -31,24 +59,7 @@ export default function ProductCard({
       )}
       {...props}
     >
-      <div className="absolute right-4 top-4 z-10">
-        <Button
-          variant="text"
-          size="icon"
-          title="Добавить в избранное"
-          className={cn(
-            `text-primary/30 transition hover:text-primary/50 hover:scale-110`,
-            heartBeat && "animate-[heart-pulse_ease-in-out_0.5s]"
-          )}
-          onClick={(e) => {
-            setHeartbeat(true);
-            setFavorite(!isFavorite);
-          }}
-          onAnimationEnd={(e) => setHeartbeat(false)}
-        >
-          <HeartIcon fill={isFavorite ? "hsl(var(--accent))" : "transparent"} />
-        </Button>
-      </div>
+      <ProductCardFavorite item={item} />
 
       <div className="relative w-full aspect-square">
         <ImageWithFallback
@@ -60,7 +71,7 @@ export default function ProductCard({
         />
       </div>
       <div className="text-center">
-        <h1 className="">{item.name}</h1>
+        <h1>{item.name}</h1>
         <p className="text-sm font-light text-black/50">
           Италия, красное, сухое, 0.75л
         </p>
