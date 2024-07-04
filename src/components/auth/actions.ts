4f1953873ec8ff66f103d4c7145a908f46081817
StarPandaBeg <kshmurakov@mail.ru>
2024-07-04 18:30:26 +0300

    fix: specify action return type

--- src/components/auth/actions.ts
+++ src/components/auth/actions.ts
@@ -6,11 +6,13 @@ import { signIn as doSignIn } from "@/service/auth";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-interface ActionData extends AuthCredentials {
+export interface ActionData extends AuthCredentials {
   redirect?: boolean;
 }
+export type AuthError = "credentials";
+export type ActionResult = [boolean, AuthError | null];
 
-export async function signIn(formData: FormData) {
+export async function signIn(formData: FormData): Promise<ActionResult> {
   const dataObject = Object.fromEntries(formData) as unknown as ActionData;
   const { redirect: shouldRedirect, ...credentials } = dataObject;
 
@@ -19,9 +21,9 @@ export async function signIn(formData: FormData) {
     authStorage(cookies()).set(authResult);
   } catch {
     if (shouldRedirect) redirect("/sign-in?error=credentials");
-    return false;
+    return [false, "credentials"];
   }
 
   if (shouldRedirect) redirect("/");
-  return true;
+  return [true, null];
 }
