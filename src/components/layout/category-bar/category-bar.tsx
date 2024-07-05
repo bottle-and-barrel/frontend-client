a262e2e923345e1ddd8b721c3a72029d104c6dec
StarPandaBeg <kshmurakov@mail.ru>
2024-07-05 15:18:15 +0300

    refactor: move category data query logic to category-bar

--- src/components/layout/category-bar/category-bar.tsx
+++ src/components/layout/category-bar/category-bar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -6,23 +8,39 @@ import {
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Category } from "@/service/category";
+import { Skeleton } from "@/components/ui/skeleton";
+import { cn } from "@/lib/util";
+import { KEY, all } from "@/service/category";
 import { NavigationMenuProps } from "@radix-ui/react-navigation-menu";
+import { useQuery } from "@tanstack/react-query";
 import Link from "next/link";
+import { HTMLAttributes } from "react";
 
-export interface CategoryBarProps extends NavigationMenuProps {
-  categories: Category[];
+export interface CategoryBarProps extends NavigationMenuProps {}
+
+function CategoryBarSkeleton({
+  className,
+  ...props
+}: HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div className={cn("px-2 py-4 space-x-4", className)} {...props}>
+      <Skeleton className="inline-block h-4 w-[80px]" />
+      <Skeleton className="inline-block h-4 w-[120px]" />
+      <Skeleton className="inline-block h-4 w-[95px]" />
+      <Skeleton className="inline-block h-4 w-[65px]" />
+    </div>
+  );
 }
 
-export default function CategoryBar({
-  categories,
-  ...props
-}: CategoryBarProps) {
+export default function CategoryBar({ ...props }: CategoryBarProps) {
+  const { data, isLoading } = useQuery({ queryKey: [KEY], queryFn: all });
+
+  if (isLoading) return <CategoryBarSkeleton {...props} />;
   return (
     <NavigationMenu {...props}>
       <ScrollArea type="auto" useHorizontalScroll>
         <NavigationMenuList className="pb-4">
-          {categories.map((c, i) => (
+          {data!.map((c, i) => (
             <NavigationMenuItem key={i}>
               <Link href={c.link} legacyBehavior passHref>
                 <NavigationMenuLink className={navigationMenuTriggerStyle()}>
--- src/components/layout/page-header/page-header.tsx
+++ src/components/layout/page-header/page-header.tsx
@@ -1,45 +1,14 @@
 "use client";
 
-import { Skeleton } from "@/components/ui/skeleton";
-import LoadingWrapper from "@/components/util/loading-wrapper";
-import { cn } from "@/lib/util";
-import { KEY, all } from "@/service/category";
-import { useQuery } from "@tanstack/react-query";
-import { HTMLAttributes } from "react";
 import ActionBar from "../action-bar/action-bar";
 import CategoryBar from "../category-bar/category-bar";
 import PageBottomNavigation from "./page-bottom-navigation";
 
-function CategoryBarSkeleton({
-  className,
-  ...props
-}: HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div className={cn("px-2 py-4 flex gap-4", className)} {...props}>
-      <Skeleton className="h-4 w-[80px]" />
-      <Skeleton className="h-4 w-[120px]" />
-      <Skeleton className="h-4 w-[95px]" />
-      <Skeleton className="h-4 w-[65px]" />
-    </div>
-  );
-}
-
 export default function PageHeader() {
-  const { data, isLoading, isError } = useQuery({
-    queryKey: [KEY],
-    queryFn: all,
-  });
-
   return (
     <header className="pb-2">
       <ActionBar />
-      <LoadingWrapper
-        isLoading={isLoading}
-        isError={isError}
-        skeleton={<CategoryBarSkeleton className="hidden xs:flex" />}
-      >
-        <CategoryBar className="mt-2 hidden xs:block" categories={data || []} />
-      </LoadingWrapper>
+      <CategoryBar className="mt-2 hidden xs:block" />
       <PageBottomNavigation className="xs:hidden" />
     </header>
   );
