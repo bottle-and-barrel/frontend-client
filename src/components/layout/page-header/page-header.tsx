8481ac61ea6b76ef371279ec006f2b79cb4b3f8a
StarPandaBeg <kshmurakov@mail.ru>
2024-07-05 15:19:06 +0300

    feat: prefetch category data on server
    - make page-header server component

--- src/components/layout/page-header/page-header.tsx
+++ src/components/layout/page-header/page-header.tsx
@@ -1,15 +1,24 @@
-"use client";
-
+import getQueryClient from "@/components/util/query-client";
+import { KEY, all } from "@/service/category";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import ActionBar from "../action-bar/action-bar";
 import CategoryBar from "../category-bar/category-bar";
 import PageBottomNavigation from "./page-bottom-navigation";
 
-export default function PageHeader() {
+export default async function PageHeader() {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: [KEY],
+    queryFn: all,
+  });
+
   return (
     <header className="pb-2">
-      <ActionBar />
-      <CategoryBar className="mt-2 hidden xs:block" />
-      <PageBottomNavigation className="xs:hidden" />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <ActionBar />
+        <CategoryBar className="mt-2 hidden xs:block" />
+        <PageBottomNavigation className="xs:hidden" />
+      </HydrationBoundary>
     </header>
   );
 }
